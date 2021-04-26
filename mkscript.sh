44147849286e47b8c5cadb73e76fb9689ff04a53
Martin Vartabetian <martin@inspiron-3493.fibertel.com.ar>
2021-04-26 20:02:19 -0300

    Squashed commit of the following:
    commit 813b2336ff2ff4998267c6d90be37386a24bc005
    Author: Martin Vartabetian <martin@inspiron-3493.fibertel.com.ar>
    Date:   Mon Apr 26 19:51:38 2021 -0300
    
        parameters are checked at the start of the script

--- mkscript.sh
+++ mkscript.sh
@@ -2,21 +2,19 @@
 
 # TODO
 # create "menu"
-# 1. validate that FILENAME param are valid (ex: ignore names starting with '-','.'... or contains '/','\'... etc)
-# 2. analize all parameters beforehand
-# 3. add dir parameter as an option for where to create the scripts
-# 4. make the "menu" loop to search for the -h parameter in any place of the param list without losing all the other values
+# 1. add dir parameter as an option for where to create the scripts
 
 function displayHelp(){
     echo "creates N° of script files with the initial lines mandatory initial lines added and proper permissions granted to the file"
     echo "Usage: mkscript.sh FILENAME..."
     echo "       mkscript.sh -h"
+    exit
 }
 
 while (( "$#" )); do
     case $1 in
-        -h|--help) displayHelp; exit;;
-        *) break;;
+        -h|--help) displayHelp; shift;;
+        *) ARRAY+=("$1"); shift;;
     esac
 done
 
@@ -44,14 +42,10 @@ function validateFileName () {
     fi
 }
 
-while (( "$#" )); do
-    FILE=$1 # get the name for the file
-    shift # shift to the next value for the next file if any
-
-    #if fc validateFileName returns 1 FileName is invalid
+for FILE in ${ARRAY[@]}; do
     validateFileName ${FILE}
     if [[ $? == 0 ]]; then
-        # check if a script/command exists with the same name, if it doesnt it creates the file, changes permissions and inserts the first line for the script 
+        # check if a script/command exists with the same name, if it doesnt it creates the file, changes permissions and inserts the first line for the script
         if [[ ! ( (-e /home/${USER}/bin/${FILE}) || (-e /bin/${FILE}) || (-e /sbin/${FILE}) || (-e /usr/bin/${FILE}) ) ]]; then
             checkFileExtension $FILE
         else
